ed4489f587c702c9c16c34a010c1600ff2dfddf8
Alexis BF <dreamer34_2@hotmail.com>
2021-06-09 16:49:15 +0200

    Error on injection ProductService -> Product/Index

---
+++ McKingApp/Controllers/ProductController.cs
@@ -0,0 +1,94 @@
+﻿using DomainModel;
+using McKingApp.Repository.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace McKingApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ICrud<Product> repository;
+        public ProductController(ICrud<Product> repository)
+        {
+            this.repository = repository;
+        }
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            return View(this.repository.ReadAll() is null ? new List<Product>() : this.repository.ReadAll().ToList());
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
